7d7982fc753d9ef3b8dbee9503005dcec56bf151
Altovisk09 <ericaltao13@gmail.com>
2025-09-18 19:33:42 -0300

    refactor(repository): Interface genérica modificada.

--- src/main/java/com/eric/skilltrack/repository/BaseRepository.java
+++ src/main/java/com/eric/skilltrack/repository/BaseRepository.java
@@ -3,10 +3,10 @@ package com.eric.skilltrack.repository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BaseRepository<T, ID> {
-    Optional<T> findById(ID id);
-    List<T> findAll();
-    void save(T entity);
-    void update(T entity);
-    void delete(ID id);
+public interface BaseRepository<T> {
+    List<T> findAll(String sheetName);
+    Optional<T> findById(String sheetName, String id);
+    void save(String sheetName, T entity);
+    void update(String sheetName, String id, T entity);
+    void delete(String sheetName, String id);
 }
