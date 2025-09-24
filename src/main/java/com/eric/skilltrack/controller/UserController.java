e86da5bc2378312d306d6280588f3bc5dba631f1
Altovisk <ericaltao13@gmail.com>
2025-09-24 20:13:33 -0300

    refactor(controller):UserController modificado para injetar métodos via contrato de interfaces.

--- src/main/java/com/eric/skilltrack/controller/UserController.java
+++ src/main/java/com/eric/skilltrack/controller/UserController.java
@@ -2,7 +2,7 @@ package com.eric.skilltrack.controller;
 
 import com.eric.skilltrack.model.User;
 import com.eric.skilltrack.model.enums.UserRole;
-import com.eric.skilltrack.service.impl.UserServiceImpl;
+import com.eric.skilltrack.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -13,45 +13,39 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserServiceImpl UserServiceImpl;
+    private final UserService userService; //
 
-    public UserController(UserServiceImpl UserServiceImpl) {
-        this.UserServiceImpl = UserServiceImpl;
+    public UserController(UserService userService) { // <-- injeta pelo contrato
+        this.userService = userService;
     }
 
-    // GET /users -> lista todos
     @GetMapping
     public List<User> getAllUsers() throws IOException {
-        return UserServiceImpl.getAllUsers();
+        return userService.getAllUsers();
     }
 
-    // GET /users/{ldap} -> busca por LDAP
     @GetMapping("/{ldap}")
     public Optional<User> getByLdap(@PathVariable String ldap) throws IOException {
-        return UserServiceImpl.getByLdap(ldap);
+        return userService.getByLdap(ldap);
     }
 
-    // POST /users/register?ldap=...
     @PostMapping("/register")
     public User registerUser(@RequestParam String ldap) throws IOException {
-        return UserServiceImpl.registerUser(ldap);
+        return userService.registerUser(ldap);
     }
 
-    // PUT /users/{ldap}/role?role=TRAINER
     @PutMapping("/{ldap}/role")
     public User updateRole(@PathVariable String ldap, @RequestParam UserRole role) throws IOException {
-        return UserServiceImpl.updateRole(ldap, role);
+        return userService.updateRole(ldap, role);
     }
 
-    // PUT /users/{ldap}/last-session
     @PutMapping("/{ldap}/last-session")
     public void updateLastSession(@PathVariable String ldap) throws IOException {
-        UserServiceImpl.updateLastSession(ldap);
+        userService.updateLastSession(ldap);
     }
 
-    // POST /users/sync -> força sync manual (além do @Scheduled)
     @PostMapping("/sync")
     public void syncFromHc() throws IOException {
-        UserServiceImpl.syncFromHc();
+        userService.syncFromHc();
     }
 }
